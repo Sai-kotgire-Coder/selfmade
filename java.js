dcee2e1303dd97a6c1868e0c2423e980717fba67
saikotgire <saikotgire9@gmail.com>
2023-06-29 17:32:47 +0530

    initial code push

---
+++ java.js
@@ -0,0 +1,7 @@
+ 
+// var num1=prompt("enter the numbers 1 to add")
+// var num2=prompt("enter the numbers 2 to add")
+// document.write(num1+num2)
+let digit1=10
+let digit2=10
+document.write("the sum of values is ",(digit1+digit2))
